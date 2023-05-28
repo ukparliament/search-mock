0cdc0122f2d6b8a7e0be5a903fa0189a73bfec0e
fantasticlife <michael@smethur.st:wq>
2023-05-28 22:04:27 +0100

    proper json rendering

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,11 +4,40 @@ class SearchController < ApplicationController
   end
   
   def form
+    json = {
+      "title": "this is some pretend configuration json that might surface available facets and etc."
+    }
+    render( :json => json, status: :ok)
   end
   
   def results
+    json = {
+      "results": [
+        {
+          "title": "This is a pretend result title",
+          "description": "This is a pretend result description.",
+          "link": "https://www.youtube.com/@harrysgarage"
+        },
+        {
+          "title": "This is a second pretend result title",
+          "description": "This is a second pretend result description.",
+          "link": "https://www.youtube.com/@DougDeMuro"
+        }
+      ],
+      "result_set":{
+        "result_count": 357,
+        "limit": 30,
+        "offset": 10
+      }
+    }
+    render( :json => json, status: :ok)
   end
   
   def object
+    json = {
+      "title": "this is a pretend object title in some pretend json",
+      "description": "this is a pretend object description in some pretend json"
+    }
+    render( :json => json, status: :ok)
   end
 end
