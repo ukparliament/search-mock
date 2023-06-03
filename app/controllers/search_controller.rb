b59cb1684167d9c1a7f1b4a358434c211c1c527e
fantasticlife <michael@smethur.st:wq>
2023-06-03 13:53:32 +0100

    results method now publishes json and xml

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,14 @@
 # We require the example XML.
 require 'xml/results/written-statements/solr.rb'
 
+# We require the example JSON.
+require 'json/results/written-statements/solr.rb'
+
 # We include the example XML modules.
 include WRITTEN_STATEMENT_SOLR_RESULTS_XML
+
+# We include the example JSON modules.
+include WRITTEN_STATEMENT_SOLR_RESULTS_JSON
 
 class SearchController < ApplicationController
   
@@ -22,19 +28,36 @@ class SearchController < ApplicationController
     # We get the document type from the URL parameter.
     document_type = params[:document_type]
     
-    # We check the document type, load the associated XML ...
-    case document_type
-    when 'written-statements'
-      xml = written_statment_results_xml
+    # We check the requested format.
+    respond_to do |format|
+      
+      # If the format requested is JSON ...
+      format.json {
+        
+        # ... we check the document type and load the appropriate JSON.
+        case document_type
+        when 'written-statements'
+          json = written_statment_results_json
+        end
+        
+        # We render the JSON.
+        render( :json => json, status: :ok)
+      }
+      
+      # If the format requested is XML ...
+      format.xml {
+        
+        # ... we check the document type and load the appropriate XML.
+        case document_type
+        when 'written-statements'
+          xml = written_statment_results_xml
+        end
+        
+        # We render the XML.
+        render( :xml => xml, status: :ok)
+      }
     end
-    
-    # ... and render as XML.
-    render( :xml => xml, status: :ok)
   end
-  
-  
-  
-  
   
   def object
     object = params[:object].to_i
