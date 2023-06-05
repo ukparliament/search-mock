3768a393bae7b2e0128bb8d7562f07628481beef
fantasticlife <michael@smethur.st:wq>
2023-06-05 11:25:26 +0100

    removed import of json and xml modules from lib, removed respond to, just based on requested document type
    '

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,3 @@
-# We require the example XML.
-require 'xml/results/written-statements/solr.rb'
-
-# We require the example JSON.
-require 'json/results/written-statements/solr.rb'
-
-# We include the example XML modules.
-include WRITTEN_STATEMENT_SOLR_RESULTS_XML
-
-# We include the example JSON modules.
-include WRITTEN_STATEMENT_SOLR_RESULTS_JSON
-
 class SearchController < ApplicationController
   
   def index
@@ -28,35 +16,11 @@ class SearchController < ApplicationController
     # We get the document type from the URL parameter.
     document_type = params[:document_type]
     
-    # We check the requested format.
-    respond_to do |format|
-      
-      # If the format requested is JSON ...
-      format.json {
-        
-        # ... we check the document type and load the appropriate JSON.
-        case document_type
-        when 'written-statements'
-          json = written_statment_results_json
-        end
-        
-        # We render the JSON.
-        render( :json => json, status: :ok)
-      }
-      
-      # If the format requested is XML ...
-      format.xml {
-        
-        # ... we check the document type and load the appropriate XML.
-        case document_type
-        when 'written-statements'
-          xml = written_statment_results_xml
-        end
-        
-        # We render the XML.
-        render( :xml => xml, status: :ok)
-      }
-    end
+    # We substitute the hyphen as supplied in the URL, for an underscrore as used in the template name.
+    document_type.gsub!( '-', '_' )
+    
+    # We render the appropriate template based on the document type and the format requested.
+    render :template => "search/#{document_type}_results"
   end
   
   def object
