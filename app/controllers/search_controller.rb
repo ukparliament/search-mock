87463d6f3af2304e53095dbd3928e8b114c4296a
fantasticlife <michael@smethur.st:wq>
2023-06-08 10:48:31 +0100

    added object view such. changed location of result templates

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,13 @@
+# # The one and only search controller.
 class SearchController < ApplicationController
   
+  
+  # ## A method to render the home page.
   def index
   end
   
+  
+  # ## A method to render the search form.
   def form
     json = {
       "title": "this is some pretend configuration json that might surface available facets and etc.",
@@ -11,6 +16,7 @@ class SearchController < ApplicationController
     render( :json => json, status: :ok)
   end
   
+  # ## A method to render a results list view.
   def results
     
     # We get the document type from the URL parameter.
@@ -20,26 +26,38 @@ class SearchController < ApplicationController
     document_type.gsub!( '-', '_' )
     
     # We render the appropriate template based on the document type and the format requested.
-    render :template => "search/#{document_type}_results"
+    render :template => "search/results/#{document_type}/results"
   end
   
+  # ## A method to render an object view.
   def object
-    object = params[:object].to_i
-    if object == 1
-      json = {
-        "id": 1,
-        "title": "This is a pretend result title",
-        "description": "This is a pretend result description.",
-        "link": "https://www.youtube.com/@harrysgarage"
-      }
-    elsif object == 2
-      json = {
-        "id": 2,
-        "title": "This is a second pretend result title",
-        "description": "This is a second pretend result description.",
-        "link": "https://www.youtube.com/@DougDeMuro"
-      }
-    end
-    render( :json => json, status: :ok)
+    
+    # We get the object uri from the URL object parameter.
+    object_uri = params[:object]
+    
+    # We construct the path to the template file.
+    file_path = construct_object_file_path( object_uri )
+    
+    # We render the appropriate object template.
+    render :template => "search/objects/#{file_path}/object"
+  end
+  
+  # ## A method to construct an object file path.
+  def construct_object_file_path( object_uri )
+    
+    # We parse the object URI as a Ruby URI.
+    object_uri = URI.parse( object_uri )
+    
+    # We extract the path from the URI.
+    file_path = object_uri.path
+    
+    # We swap out any slashes for hyphens.
+    file_path.gsub!( '/', '-' )
+    
+    # We strip out the first hyphen.
+    file_path = file_path[1..-1]
+    
+    # We return the file path.
+    file_path
   end
 end
